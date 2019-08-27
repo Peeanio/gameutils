36e9c91ab9709558941fdde5049bde50eac28980
Peeanio <maxandsam9@gmail.com>
2019-08-26 22:45:26 -0700

    modified:   cyberchargen.c

--- cyberchargen.c
+++ cyberchargen.c
@@ -20,22 +20,31 @@ int rollStat()
 	int statDice = 2;
 	int rollNum = 0;
 	srand(time(0));
-	int total = 0;
+	long total = 0;
 	for( rollNum = 0; rollNum < statDice; rollNum++){
 //	rolls dice and adds them up
 		int dieRoll = rollDie();
-		printf("die roll: %d\n", dieRoll);
-		total += dieRoll; // issue was here. was redeclaring total
-		printf("total: %d\n", total);
-
+//		printf("die roll: %d\n", dieRoll);
+		total += dieRoll; 
+//		printf("total: %d\n", total);
 		}
-	printf("final: %d\n", total);
+//	printf("final: %d\n", total);
+	return total;
 //	prints grand total 
 }
 
 int main() {
 // rolls for stat	
+	int statInt = rollStat();
+	int statRef = rollStat();
+	int statTech = rollStat();
+	int statCool = rollStat();
+	int statAttr = rollStat();
+	int statLuck = rollStat();
+	int statMA = rollStat();
+	int statBody = rollStat();
+	int statEmp = rollStat();
+	printf("Int is: %d\n", statInt);
+	printf("Ref is: %d\n", statRef);
 
-	int stat = rollStat();
-	printf("stat is: %d\n", stat);
 }
