70bcb35364fc67cc3bbc7ab4e12294aa59b26b40
Peeanio <maxandsam9@gmail.com>
2019-09-16 00:15:36 -0700

    modified:   chargen.sh

--- chargen.sh
+++ chargen.sh
@@ -11,12 +11,42 @@ Roll () {
 
 RollStat () {
 #take argument name of stat
-	STAT=$(Roll 6)
-	echo $STAT
-	STAT=$STAT+$(Roll 6))
+	STAT=$(($(Roll 6)+$(Roll 6)))
+#not ready for 11+ reroll yet, likely because of string instead of int
+#	if [ $STAT >= 11 ]; then
+#		STAT=$(($(Roll 6)+$(Roll 6)))
+#	fi
 	echo $1 is: $STAT
-
+	
 ##$(Roll 10)+$(Roll 10)
+}
+RollClass () {
+#rolls for one of the classes in game
+	CLASS=$(Roll 10)
+	if [ $CLASS == 1 ]; then
+		CLASSNAME="Rockerboy"
+	elif [ $CLASS == 2 ]; then
+		CLASSNAME="Solo"
+	elif [ $CLASS == 3 ]; then
+		CLASSNAME="Netrunner"
+	elif [ $CLASS == 4 ]; then
+		CLASSNAME="Techie"
+	elif [ $CLASS == 5 ]; then
+		CLASSNAME="Medtech"
+	elif [ $CLASS == 6 ]; then
+		CLASSNAME="Media"
+	elif [ $CLASS == 7 ]; then
+		CLASSNAME="Cop"
+	elif [ $CLASS == 8 ]; then
+		CLASSNAME="Corporate"
+	elif [ $CLASS == 9 ]; then
+		CLASSNAME="Fixer"
+	elif [ $CLASS == 10 ]; then
+		CLASSNAME="Nomad"
+	else
+		CLASSNAME="WTF"
+	fi
+	echo "Class is "$CLASSNAME
 }
 
 RollStat INT 
@@ -27,5 +57,4 @@ RollStat LK
 RollStat ATT
 RollStat MA
 RollStat EMP
-
-
+RollClass
