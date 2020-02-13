0ce25ebf1108fb377212a556a57f1aef95057322
Peeanio <maxandsam9@gmail.com>
2020-02-12 22:10:24 -0800

    modified:   chargen.py 	worked on cyberware rolls.needs work, but saving progress

--- chargen.py
+++ chargen.py
@@ -20,6 +20,8 @@ def rollStat(stat):
 	print(stat + " is: " + str(statScore))
 	
 def rollBackstory():
+#generates the player class and some fluff to make more interesting
+	global playerClass #this makes it available outside the functions
 	playerClass = arrayClasses[rollDx(9)]
 	playerClothes = arrayClothes[rollDx(9)]
 	playerEthnicity = arrayEthnicity[rollDx(9)]
@@ -29,10 +31,31 @@ def rollBackstory():
 	print("Ethnicity is: " + str(playerEthnicity))
 	print("Parents are/were: " + str(playerParents))
 	
+def rollCyberWare():
+#rolls for cybernetics for character
+	rolledCyberware = [0 for x in range(5)]
+	if str(playerClass) == str("Solo"):
+		#diceCyberWare = [ 0 for x in range(5) ]
+		for x in range(5):
+			rolledCyberware[x] = rollDx(10)
+			Print("Cyberware is: " + str(rolledCyberware[x]))
+		
+	else:
+		#diceCyberWare = [ 0 for x in range(2) ]
+		for x in range(2):
+			rolledCyberware[x] = rollDx(10)
+			print("Cyberware is: " + str(rolledCyberware[x]))
+	playerCyberWareRolls = rolledCyberware
+	print(str(rolledCyberware))
+	#need to break this part up into individual rolls, and append to character
 def genCharacter():
 	for x in range(0,len(statsValues)):
 		statsValues[x] = rollStat(statsLongNames[x])
 	rollBackstory()
+	rollCyberWare()
+	
+	
+	
 	
 ########################################################################
 #declare arrays
@@ -53,6 +76,21 @@ statsLongNames=["Intelligence", "Reflex", "Technology", "Cool", "Luck",\
 statsShortNames=["int", "ref", "tech", "cool", "luck", "att", "ma", \
 "emp", "body"]
 statsValues=[0 for x in range(9)]
+arrayCyberWare=["Cyberoptics", "Cyberarm with gun", "Cyberaudio",\
+ "Big Nucks", "Rippers", "Vampires", "Slice n' Dice",\
+ "Reflex Boost (Kerenzikov)", "Reflex Boost (Sandevistan)", "Nothing"]
+arrayCyberoptics=["Infrared", "Lowlight", "Camera", "Dartgun",\
+ "Antidazzle", "Targeting Scope"]
+arrayCyberarm=["Med. Pistol", "Light Pistol", "Med. Pistol",\
+ "Light Submachinegun", "Very Heavy Pistol", "Heavy Pistol"]
+arrayCyberaudio=["Wearman", "Radio Splice", "Phone link",\
+ "Amplified Hearing", "Sound Editing", "Digital Recording Link"]
+arrayArmor=["Heavy Leather", "Armor Vest", "Light Armor Jacket",\
+ "Light Armor Jacket", "Med Armor Jacket", "Med Armor Jacket",\
+ "Heavy Armor Jacket", "Heavy Armor Jacket", "Metalgear"]
+arrayWeapons=["Knife", "Light pistol", "Med pistol", "Heavy Pistol"\
+ "Heavy Pistol", "Light SMG", "Lt. Assault Rifle", "Med Assault Rifle",\
+  "Hvy. Assault rifle", "Heavy Assault Rifle"]
 
 ########################################################################
 #main
