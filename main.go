b3cc3b1f8a09f17772b59bd83d73decf3e4b8ba5
Peeanio <maxandsam9@gmail.com>
2021-07-02 09:55:32 -0700

    inital commit

---
+++ main.go
@@ -0,0 +1,112 @@
+// Frostgrave character creator
+
+// Max Russell
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+//	"strconv"
+)
+
+//classes w/ oposition
+//chronomancer 
+//  +2 necro sooth ele
+//  +4 tha summ ill witch sigil
+//  +6 enchant
+
+//elementalist
+//  +2 summ en chr
+//  +4 tha sooth sig wit nec
+//  +6 ill
+
+//enchanter
+//  +2 wit sigil ele
+//  +4 nec ill sum sooth tha
+// chrono
+
+//illusionist
+//  +2 sooth sigil tha
+//  +4 nec wit chro summ enc
+//  +6 ele
+
+//necromancer
+//  +2 wit chro summ
+//  +4 ele sig ill enc soot
+//  +6 tha
+
+//sigilist
+//  +2 tha ill enc
+//  +4 nec ele wit chro soo
+//  +6 summ
+
+//soothsayer
+//  +2 tha chro ill
+//  +4 ench summ necr ele sigil
+//  +6 witch
+
+//summoner
+//  +2 necro witch ele
+//  +4 sooth enc ill chro tha
+//  +6 sigi
+
+//thaumaturge
+//  +2 soot sigil ill
+//  +4 ele witch chro summ ench
+//  +6 necro
+
+//witch
+//  +2 enc necr summ
+//  +4 tha ill ele sigi chro
+//  +6 soot
+
+//spells: three own school, one each aligned, two neautral (different)
+
+//stats: move (m) fight (f) shoot (s) armour (a) will (w) health (h)
+//starting wizard: 6 +2 +0 10 +4 14
+
+//start w/ 500gc
+//200gc apprentice
+// 6 w-2 w-2 10 w-2 w-4
+// 6 +0 -2 10 +2 10
+
+//war hound 10gc
+//thug 20gc
+//thief 20gc
+//archer 50gc
+//crossbowman 50gc
+//infantryman 50gc
+//tracker 80gc
+//man-at-arms 80gc
+//treasure hunter 80gc
+//knight 100gc
+//templar 100gc
+//ranger 100gc
+//barbarian 100gc
+//apothecary 100gc
+//marksman 100gc
+
+func rollDx(x int) int {
+//rolls a die up to the number specified
+	min := 1
+	max := x + 1
+	return rand.Intn(max - min) + min
+
+}
+
+func rollTox(x int) int {
+//rolls from zero to number specified
+	return rand.Intn(x)
+}
+
+func main() {
+//	fmt.Println("Hello world")
+	rand.Seed(time.Now().UnixNano())
+//	fmt.Println(time.Now().UnixNano())
+//	rand.Seed(42)
+//	number := rand.Intn(10)
+	//numstring := strconv.Itoa(number)
+	fmt.Println(rollTox(10))
+}
