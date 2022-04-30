790a15e457730039297a39f69be53fa0ac1db59c
max <maxandsam9@gmail.com>
2022-04-30 15:36:54 -0700

    getting started with the api, but first some tweaks on roll.py to remove clutter, and only show the dice above a set value 	modified:   main.go 	modified:   roll.py

--- roll.py
+++ roll.py
@@ -14,14 +14,20 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-d", type="int", dest="numOfDice", help="number of dice to be rolled")
 parser.add_option("-s", type="int", dest="sizeOfDice", help="number of sides dice have, default six")
+parser.add_option("-a", type="int", dest="aboveInt", help="only return results above this value")
 (options, args) = parser.parse_args()
 
 def rollDice(y):
     resultList = [];
     for x in range(y):
-        resultList.append( random.randint(1,6) )
+        resultList.append( random.randint(1,sizeOfDice) )
     for x in range(1, sizeOfDice):
-        print("Number of " + str(x) + "s: " + str(resultList.count(x)) )
+        if resultList.count(x) != 0:
+            if options.aboveInt is not None:
+                if x >= options.aboveInt:
+                    print("Number of " + str(x) + "s: " + str(resultList.count(x)) )
+            else:
+                print("Number of " + str(x) + "s: " + str(resultList.count(x)) )
 
 if options.sizeOfDice is None:
     sizeOfDice = 7
