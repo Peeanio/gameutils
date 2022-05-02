94b7d785013a595c69b17712d3b03160d489bbef
max <max@theguards.net>
2022-05-01 17:35:30 -0700

    testing using net/http, nothing third party. to streamline in the future, in case things get expanded, something that can handle that will be tested and used 	modified:   main.go

--- main.go
+++ main.go
@@ -5,11 +5,22 @@
 package main
 
 import (
-//		"fmt"
+		"fmt"
+		"encoding/json"
 		"net/http"
 		"io"
+		"log"
 )
-		
+
+type Operation struct {
+	ID string `json:"ID"`
+	Title string `json:"Title"`
+	Agent string `json:"Agent"`
+	Status string `json:"Status"`
+}
+
+var Operations []Operation
+
 func hello(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set(
 		"Content-Type",
@@ -27,9 +38,37 @@ func hello(res http.ResponseWriter, req *http.Request) {
 			</body>
 		</html>`,
 	)
+	fmt.Println("Endpoint hit: hello")
 }
 
+func handleRequests() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/operations", returnAllOperations)
+//	mux.HandleFunc("/operation/{id}", returnSingleOperation)
+	//log.Fatal(http.ListenAndServe(":9000", nil))
+	s := &http.Server{
+		Addr: ":9000",
+		Handler: mux,
+	}
+	log.Fatal(s.ListenAndServe())
+}
+
+func returnAllOperations(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint hit: returnAllOperations")
+	json.NewEncoder(w).Encode(Operations)
+}
+
+//func createNewOperation(w http.ResponseWriter, r *http.Request) {
+//	reqBody, _ := ioutil.ReadAll(r.body)
+//	fmt.Fprintf(w, "%+v", string(reqBody))
+//}
+
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":9000", nil)
+	fmt.Println("HTTP API - Espionage v1")
+	Operations = []Operation{
+		Operation{ID: "1", Title: "SECOND CANCEL", Agent: "Jada Wiseman", Status: "Register"},
+		Operation{ID: "2", Title: "TAUT BIRTHDAY", Agent: "Jaroslav Maanan", Status: "Choclate"},
+	}
+	handleRequests()
 }
