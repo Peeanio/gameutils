f2ec21235811e9f90dd2a309efa1801e20adf230
max <max@theguards.net>
2023-04-04 08:20:56 -0700

    tweaked to run correctly

--- campaign_creation_tool.py
+++ campaign_creation_tool.py
@@ -4,6 +4,8 @@
 """solo wargaming campaign-inspired creation tool"""
 
 import argparse
+import os
+import random
 import requests
 
 parser = argparse.ArgumentParser(prog='campaign_creation_tool.py', \
@@ -16,10 +18,23 @@ args = parser.parse_args()
 
 def generate_map():
     """generates the campaign map"""
-    print("map")
+    if os.path.exists('map.png'):
+        print('file exists')
+    else:
+        max_x = 15
+        max_y = 15
+        grid_type = "hex"
+        seed = random.randint(100000000, 999999999)
+        payload = {"more": 0, "maxx": max_x, "maxy": max_y, "type": grid_type, "seed": seed}
+        resp = requests.get('https://tools.dehumanizer.com/mapgen/map.php', \
+        params=payload, stream=True, timeout=10)
+        if resp.ok:
+            with open('map.png', 'wb') as new_file:
+                new_file.write(resp.content)
+                print('file written')
 
 if __name__ == '__main__':
     if args.command_arg == 'map':
         generate_map()
-
-#https://tools.dehumanizer.com/mapgen/index2/php?more=0&maxx=15&maxy15&type=hex&seed=601147581
+    else:
+        parser.print_help()
